52b62bdf646b92a2bc731cb6a1f1894f2e354715
francoisxavier <francois-xavier1.leprovost-de-saint-jean@supinfo.com>
2025-03-26 17:21:08 +0100

    refactor booking and search pages; update event data handling and improve UI elements

--- Frontend-4WEBD/src/components/booking-ticket-page.jsx
+++ Frontend-4WEBD/src/components/booking-ticket-page.jsx
@@ -1,27 +1,18 @@
 import {useNavigate, useParams} from "react-router-dom";
-import {CheckCircleIcon, LoaderCircleIcon, MapPinIcon} from "lucide-react";
-import {useState} from "react";
+import {CheckCircleIcon, LoaderCircleIcon} from "lucide-react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import {useAuth} from "../hooks/useAuth.js";
 
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
 
 export default function BookingTicketPage() {
     const {user, token} = useAuth();
     const navigate = useNavigate();
-    /*const [event, setEvent] = useState(null);*/
-    const [event, setEvent] = useState({
-        id: 1,
-        name: "Event name",
-        date: "31/12/2026",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
-        picture: "https://picsum.photos/500/300",
-        price: 10,
-    });
+    const [event, setEvent] = useState({id: 0, name: "", description: "", location: "", date: "", image: ""});
     const [loading, setLoading] = useState(false);
     const {id} = useParams();
 
-    const [eventDate, setEventDate] = useState("");
     const [people, setPeople] = useState(1);
     const [errorMessage, setErrorMessage] = useState(null);
     const [successMessage, setSuccessMessage] = useState(null);
@@ -30,15 +21,9 @@ export default function BookingTicketPage() {
 
     const handleBooking = () => {
 
-        // Calculer le prix total
-        setTotalPrice(event.price * people);
-
-        axios.post(apiUrl + "/reservations/create", {
-            user_id: user.id,
-            event_id: event.id,
-            people,
-            total_price: totalPrice,
-            status: "waiting"
+        axios.post(apiUrl + "/TicketService/api/Ticket", {
+            eventId: event.id,
+            numberOfPlaces: people,
         }, {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -55,25 +40,23 @@ export default function BookingTicketPage() {
                 setTimeout(() => setErrorMessage(null), 2000);
             });
     }
-    /* useEffect(() => {
-         setTotalPrice(event.price * people);
-     }, [people]);*/
 
 
-    /*useEffect(() => {
+    useEffect(() => {
         if (!user) {
             navigate("/login");
             return;
         }
         // Récupérer les données de l'évènement
-        axios.get(`${apiUrl}/event/search`, {
+        axios.get(`${apiUrl}/EventService/Events/get/${id}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
             params: {id}
         })
             .then((response) => {
-                const eventData = response.data[0];
+                const eventData = response.data;
+                console.log(eventData);
 
                 setEvent(eventData);
                 setLoading(false);
@@ -82,7 +65,7 @@ export default function BookingTicketPage() {
                 console.error(error);
                 setLoading(false);
             });
-    }, [id]);*/
+    }, [id]);
 
     if (loading) {
         return (
@@ -126,7 +109,8 @@ export default function BookingTicketPage() {
             {!successMessage && (
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
                     {errorMessage && (
-                        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                        <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4"
+                             role="alert">
                             <strong className="font-bold">Erreur : </strong>
                             <span className="block sm:inline">{errorMessage}</span>
                         </div>
@@ -134,18 +118,26 @@ export default function BookingTicketPage() {
 
                     <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                         <div>
-                            <img src={event.picture} alt={event.name} className="w-full h-96 object-cover rounded-lg shadow-md"/>
+                            <img src={`${apiUrl}/EventService${event.image}`}
+                                 alt={event.name} className="w-full h-96 object-cover rounded-lg shadow-md"/>
                         </div>
                         <div>
                             <div className="flex items-center justify-between mb-4">
                                 <h1 className="text-3xl font-bold text-indigo-600">{event.name}</h1>
                                 {totalPrice && (
-                                    <p className="text-gray-500">Prix total: <span className="font-semibold text-gray-900">{totalPrice}€</span></p>
+                                    <p className="text-gray-500">Prix total: <span
+                                        className="font-semibold text-gray-900">{totalPrice}€</span></p>
                                 )}
                             </div>
                             <p className="text-gray-700 mb-4">{event.description}</p>
-                            <p className="text-gray-700 mb-4">Prix par personne: <span className="font-semibold text-gray-900">{event.price}€</span></p>
-                            <label className="block text-gray-700 mb-4">Date: <span className="font-semibold text-gray-900">{event.date}</span></label>
+                            <p className="text-gray-700 mb-4">Localisation: <span
+                                className="font-semibold text-gray-900">{event.location}€</span></p>
+                            <p className="block text-gray-700 mb-4">Le <span
+                                className="font-semibold text-gray-900">{new Date(event.date).toLocaleDateString('fr-FR')}</span> à <span
+                                className="font-semibold text-gray-900">{new Date(event.date).toLocaleTimeString('fr-FR', {
+                                hour: '2-digit',
+                                minute: '2-digit'
+                            })}</span></p>
                             <div className="flex items-center mb-4">
                                 <label className="block w-1/4 text-gray-700">Nombre de personnes:</label>
                                 <input
@@ -156,8 +148,10 @@ export default function BookingTicketPage() {
                                     onChange={(e) => setPeople(e.target.value)}
                                 />
                             </div>
-                            <button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-300 ease-in-out transform hover:scale-105"
-                                    onClick={handleBooking}>Réserver {eventDate && (`pour ${totalPrice}€`)}</button>
+                            <button
+                                className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-300 ease-in-out transform hover:scale-105 cursor-pointer"
+                                onClick={handleBooking}>Réserver
+                            </button>
                         </div>
                     </div>
                 </div>
--- Frontend-4WEBD/src/components/search-page.jsx
+++ Frontend-4WEBD/src/components/search-page.jsx
@@ -1,27 +1,23 @@
 "use client"
 import {useEffect, useState} from "react";
-import {TicketIcon, SearchIcon, UsersIcon} from "lucide-react"
-import {Menu} from "@headlessui/react"
+import {CalendarIcon, MapPinIcon, SearchIcon, TicketIcon} from "lucide-react"
 import {StarIcon as SolidStarIcon} from '@heroicons/react/24/solid'
 import {StarIcon as OutlineStarIcon} from "@heroicons/react/24/outline";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {useAuth} from "../hooks/useAuth.js";
 
 export default function SearchPage() {
-    const apiUrl = import.meta.env.VITE_API_URL;
+    const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+    const {token} = useAuth();
     const navigate = useNavigate();
-    const [destination, setDestination] = useState("")
+    //Paramètres de recherche
+    const [eventTitle, setEventTitle] = useState("")
+    const [eventLocation, setEventLocation] = useState("")
     const [eventDate, setEventDate] = useState("")
-    const [guests, setGuests] = useState("2")
-    const [priceRange, setPriceRange] = useState([50, 500])
+
+
     const [limit, setLimit] = useState(10)
-    const [amenitiesFilter, setAmenitiesFilter] = useState({
-        wifi: false,
-        restaurant: false,
-        pool: false,
-        parking: false,
-        gym: false
-    })
     const [events, setEvents] = useState([]);
 
 
@@ -32,29 +28,27 @@ export default function SearchPage() {
 
     const fetchEvents = () => {
         const params = {};
-        if (destination !== "") {
-            params.destination = destination;
+        if (eventTitle !== "") {
+            params.title = eventTitle;
+        }
+        if (eventLocation !== "") {
+            params.location = eventLocation;
         }
         if (eventDate !== "") {
-            params.eventDate = eventDate;
+            params.date = eventDate;
         }
 
-        if (guests !== "") {
-            params.guests = guests;
-        }
-        if (priceRange[1] !== 500) {
-            params.price = priceRange[1];
-        }
         if (limit !== 10) {
             params.limit = limit;
         }
-        if (Object.values(amenitiesFilter).some(Boolean)) {
-            params.amenities = JSON.stringify(Object.keys(amenitiesFilter).filter((key) => amenitiesFilter[key]));
-        }
-
 
         axios
-            .get(`${apiUrl}/events/search`, {params})
+            .get(`${apiUrl}/EventService/Events/getall`,  {
+                params,
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
             .then((response) => {
                 const eventData = response.data;
                 setEvents(eventData);
@@ -64,13 +58,6 @@ export default function SearchPage() {
             })
     }
 
-    const clearSearch = () => {
-        setDestination("")
-        setEventDate("")
-        setGuests("2")
-        setPriceRange([50, 500])
-        setAmenitiesFilter({wifi: false, restaurant: false, pool: false, parking: false, gym: false})
-    }
 
     const handleReservation = (eventId) => {
         navigate(`/booking/${eventId}`)
@@ -84,88 +71,68 @@ export default function SearchPage() {
             {/* Search Form */}
             <div className="bg-white shadow-md py-6 mb-8">
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <form onSubmit={handleSearch} className="grid grid-cols-1 md:grid-cols-4 gap-4">
+                    <form onSubmit={handleSearch}
+                          className="grid grid-cols-1 md:grid-cols-4 gap-4">
                         <div className="relative">
-                            <label htmlFor="destination" className="sr-only">
-                                Destination
+                            <label htmlFor="eventName" className="sr-only">
+                                Nom de l'évènement
                             </label>
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                            <div
+                                className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <TicketIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
                             </div>
                             <input
-                                id="destination"
-                                name="destination"
+                                id="eventTitle"
+                                name="eventTitle"
                                 type="text"
-                                required
                                 className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                                placeholder="Entrez un événement"
-                                value={destination}
-                                onChange={(e) => setDestination(e.target.value)}
+                                placeholder="Nom de l'évènement"
+                                value={eventTitle}
+                                onChange={(e) => setEventTitle(e.target.value)}
                             />
                         </div>
 
                         <div className="relative">
-                            <label htmlFor="event-date" className="sr-only">
-                                Date de l'évènement
+                            <label htmlFor="eventLocation" className="sr-only">
+                                Localisation
                             </label>
-                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                                <svg className="h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
+                            <div
+                                className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <MapPinIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
                             </div>
                             <input
-                                id="event-date"
-                                name="event-date"
-                                type="date"
-                                /*required*/
+                                id="eventLocation"
+                                name="eventLocation"
+                                type="text"
                                 className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                placeholder="Localisation"
+                                value={eventLocation}
+                                onChange={(e) => setEventLocation(e.target.value)}
+                            />
+                        </div>
+                        <div className="relative">
+                            <label htmlFor="eventDate" className="sr-only">
+                                Date
+                            </label>
+                            <div
+                                className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                <CalendarIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
+                            </div>
+                            <input
+                                id="eventLocation"
+                                name="eventLocation"
+                                type="date"
+                                className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                placeholder="Date"
                                 value={eventDate}
                                 onChange={(e) => setEventDate(e.target.value)}
                             />
                         </div>
 
-
-                        <div className="relative">
-                            <Menu as="div" className="relative inline-block text-left w-full">
-                                <div>
-                                    <Menu.Button
-                                        className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                                        {guests} personne{guests > 1 ? "s" : ""}
-                                        <UsersIcon className="ml-2 -mr-1 h-5 w-5 text-gray-400" aria-hidden="true"/>
-                                    </Menu.Button>
-                                </div>
-
-                                <Menu.Items
-                                    className="origin-top-right absolute z-10 right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                >
-                                    <div className="py-1">
-                                        {[1, 2, 3, 4, 5].map((number) => (
-                                            <Menu.Item key={number}>
-                                                {(active) => (
-                                                    <button
-                                                        onClick={() => setGuests(number.toString())}
-                                                        className={`${
-                                                            active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                                                        } block px-4 py-2 text-sm w-full text-left`}
-                                                    >
-                                                        {number} personne{number > 1 ? "s" : ""}
-                                                    </button>
-                                                )}
-                                            </Menu.Item>
-                                        ))}
-                                    </div>
-                                </Menu.Items>
-                            </Menu>
-                        </div>
-
                         <div>
                             <button
                                 type="submit"
-                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer"
+                                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                                 <SearchIcon className="mr-2 h-5 w-5" aria-hidden="true"/>
                                 Rechercher
@@ -178,40 +145,10 @@ export default function SearchPage() {
             {/* Main Content */}
             <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-col lg:flex-row gap-8">
-                    {/* Filters */}
-                    <div className="lg:w-1/4">
-                        <div className="bg-white shadow rounded-lg p-6">
-                            <div className="flex items-center justify-between mb-4">
-                                <h2 className="text-lg font-medium text-gray-900 ">Filtres</h2>
-                                <button className="text-indigo-600 hover:text-indigo-700 cursor-pointer "
-                                        onClick={clearSearch}>Effacer la recherche
-                                </button>
-                            </div>
-                            <div className="mb-6">
-                                <h3 className="font-medium text-gray-900 mb-2">Budget</h3>
-                                <div className="flex items-center space-x-4">
-                                    <input
-                                        type="range"
-                                        min="0"
-                                        max="1000"
-                                        value={priceRange[1]}
-                                        onChange={(e) => {
-                                            setPriceRange([priceRange[0], Number.parseInt(e.target.value)]);
-                                        }}
-                                        onMouseUp={() => fetchEvents()}
-                                        className="w-full"
-                                    />
-                                    <span className="text-sm text-gray-500">{priceRange[1]}€</span>
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div>
 
                     {/* Results */}
                     {Array.isArray(events) && events.length > 0 ? (
-                        <div className="lg:w-3/4">
+                        <div className="w-full">
                             <div className="flex items-center justify-between mb-6">
                                 <div className="flex items-center space-x-4">
                                     <h2 className="text-2xl font-bold text-gray-900">Résultats de recherche</h2>
@@ -244,7 +181,8 @@ export default function SearchPage() {
                                 {events.map((event) => (
                                     <div key={event.id} className="bg-white shadow rounded-lg overflow-hidden">
                                         <div className="relative h-48">
-                                            <img src={event.picture || "/placeholder.svg"} alt={event.name}
+                                            <img src={`${apiUrl}/EventService${event.image}`}
+                                                 alt={event.name}
                                                  className="object-cover w-full h-full"/>
                                         </div>
                                         <div className="p-6">
@@ -258,28 +196,15 @@ export default function SearchPage() {
                                                 </div>
                                                 <div
                                                     className="flex items-center bg-green-100 text-green-800 px-2 py-1 rounded-full text-sm font-medium">
-                                                    {[...Array(5)].map((_, index) => (
-                                                        index < event.rating ? (
-                                                            <SolidStarIcon
-                                                                key={index}
-                                                                className={`h-4 w-4 mr-1 text-yellow-500`}
-                                                            />
-                                                        ) : (
-                                                            <OutlineStarIcon
-                                                                key={index}
-                                                                className={`h-4 w-4 mr-1 text-yellow-500`}
-                                                            />
-                                                        )
-                                                    ))}
+                                                    <span>{event.numberOfPlaces} places disponibles</span>
                                                 </div>
                                             </div>
                                             <p className="mt-4 text-sm text-gray-600">{event.description}</p>
 
                                             <div className="mt-6 flex items-center justify-between">
                                                 <div>
-                                                    <span
-                                                        className="text-2xl font-bold text-gray-900">{event.price}€</span>
-                                                    <span className="text-gray-600 text-sm"> / nuit</span>
+                                                    <span className="text-2xl font-bold text-gray-900">{new Date(event.date).toLocaleDateString()}</span>
+                                                    <span className="block text-sm text-gray-500">à {new Date(event.date).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</span>
                                                 </div>
                                                 <button
                                                     onClick={() => handleReservation(event.id)}
